ea654978bb83d69cb38cf0004c3468c944f5e806
Adrián Asensio <aasensiomtd@gmail.com>
2026-05-09 22:19:06 +0200

    Deprecated Telescope

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -146,15 +146,6 @@ return {
   },
 
   {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    cmd = "Telescope",
-    opts = function()
-      return require "nvchad.configs.telescope"
-    end,
-  },
-
-  {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPost", "BufNewFile" },
     cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
